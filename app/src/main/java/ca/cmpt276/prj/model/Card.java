8b68a22eb19a9e01408a720e9c0c9edb9e7d5c99
Benjamin Howard <bwhoward@sfu.ca>
2020-06-30 15:40:57 -0700

    Add CardImage enum and start coding constructor for Card class.
    The isNotUniformlyThemed method is UNFINISHED.

--- app/src/main/java/ca/cmpt276/prj/model/Card.java
+++ app/src/main/java/ca/cmpt276/prj/model/Card.java
@@ -1,4 +1,56 @@
 package ca.cmpt276.prj.model;
 
 public class Card {
+    private boolean isLandscapeImageSet;  //  Might want this to be only in Deck class.
+    private CardImage topImage;
+    private CardImage middleImage;
+    private CardImage bottomImage;
+
+    public Card(boolean isLandscapeImageSet, CardImage topImage, CardImage middleImage, CardImage bottomImage) {
+        this.isLandscapeImageSet = isLandscapeImageSet;
+        if (hasDuplicateImage(topImage, middleImage, bottomImage)) {
+            throw new IllegalArgumentException("Error: All three CardImages must be unique.");
+        }
+
+        this.topImage = topImage;
+        this.middleImage = middleImage;
+        this.bottomImage = bottomImage;
+    }
+
+    private boolean hasDuplicateImage(CardImage topImage, CardImage middleImage,
+                                      CardImage bottomImage) {
+        return (topImage.equals(middleImage) || topImage.equals(bottomImage)
+                || middleImage.equals(bottomImage));
+    }
+
+    private boolean isNotUniformlyThemed(CardImage topImage, CardImage middleImage,
+                                         CardImage bottomImage) {
+        if (isInPredatorSet(topImage)) {
+            if (isLandscapeImageSet) {
+                return true;
+            }
+            else {
+                if (isInPredatorSet(middleImage)) {
+                    if (isInPredatorSet(bottomImage)) {
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+        else {
+            //  NOTE: this should have it's own proper if-else structure, not just return true.
+            //  I only set it to that so that it will compile on my commit.
+            return true;
+        }
+    }
+
+    private boolean isInPredatorSet(CardImage image) {
+        return image.compareTo(CardImage.BEAR) >= 0;
+    }
 }
---
+++ app/src/main/java/ca/cmpt276/prj/model/CardImage.java
@@ -0,0 +1,4 @@
+package ca.cmpt276.prj.model;
+
+public enum CardImage { CAVE, DESERT, FOREST, OCEAN, STORM, SUNRISE, VOLCANO,
+    BEAR, EAGLE, LEOPARD, LION, SHARK, SNAKE, SPIDER }
