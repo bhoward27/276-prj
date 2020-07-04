3dfa1831f5c893e9423dffff8edef1cd93e8938a
whorvath <whorvath@sfu.ca>
2020-07-04 01:04:14 -0700

    Remove obsolete citation

--- app/src/main/java/ca/cmpt276/prj/model/CardImage.java
+++ app/src/main/java/ca/cmpt276/prj/model/CardImage.java
@@ -6,9 +6,6 @@ import java.util.Map;
 /**
  * This enum defines the two sets of images that can appear on a card.
  * Any image not defined in this enum is invalid for a card.
- * Citation:
- * https://codingexplained.com/coding/java/enum-to-integer-and-integer-to-enum
- * Makes accessing values and enums easier
  * TODO: refactor image set selection elsewhere for future expandability
  */
 public enum CardImage { CAVE, DESERT, FOREST, OCEAN, STORM, SUNRISE, VOLCANO,
