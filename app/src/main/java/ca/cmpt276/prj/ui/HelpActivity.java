bf5b48320774c14ba2e26b87044425d5783e0180
origin <memora@sfu.ca>
2020-07-05 00:50:30 -0700

    Updated help activity and associated files.
    Minor edits, including suggestion by teammates to remove @Author annotation for help activity and associated files.

--- app/src/main/java/ca/cmpt276/prj/ui/HelpActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/HelpActivity.java
@@ -24,10 +24,9 @@ public class HelpActivity extends AppCompatActivity {
         String helpDescriptionText = getString(R.string.helpDescriptionText);
         helpDescriptionAndCitation.setText(helpDescriptionText);
 
-        /*
-        Since a lot of text is used here, I wanted to make it scroll. I used code from this link, which allows such scrolling wihtout a Scrollview.
-        From https://stackoverflow.com/questions/1748977/making-textview-scrollable-on-android
-         */
+
+        //Since a lot of text is used here, I wanted to make it scroll. I used code from this link, which allows such scrolling wihtout a Scrollview.
+        //From https://stackoverflow.com/questions/1748977/making-textview-scrollable-on-android
         helpDescriptionAndCitation.setMovementMethod(new ScrollingMovementMethod());
 
         }
