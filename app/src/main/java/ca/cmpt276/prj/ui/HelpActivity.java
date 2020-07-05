589cc16ec70ab069a2ff066eeb49e579e01bf190
origin <memora@sfu.ca>
2020-07-05 00:35:55 -0700

    Updated help activity and associated files.
    Minor edits, including suggestion by teammates to remove @Author annotation for help activity and associated files.

--- app/src/main/java/ca/cmpt276/prj/ui/HelpActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/HelpActivity.java
@@ -3,8 +3,15 @@ package ca.cmpt276.prj.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import ca.cmpt276.prj.R;
+
+
+/**
+ * Help Screen for showcasing instructions on how to play the game.
+ */
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -12,5 +19,16 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        TextView helpDescriptionAndCitation = findViewById(R.id.helpDescription);
+        String helpDescriptionText = getString(R.string.helpDescriptionText);
+        helpDescriptionAndCitation.setText(helpDescriptionText);
+
+        /*
+        Since a lot of text is used here, I wanted to make it scroll. I used code from this link, which allows such scrolling wihtout a Scrollview.
+        From https://stackoverflow.com/questions/1748977/making-textview-scrollable-on-android
+         */
+        helpDescriptionAndCitation.setMovementMethod(new ScrollingMovementMethod());
+
     }
 }
