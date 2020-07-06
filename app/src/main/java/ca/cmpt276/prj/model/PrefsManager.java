5f4060e423bcdc88902586582ffbb2734fac0b3b
whorvath <whorvath@sfu.ca>
2020-07-06 12:35:53 -0700

    Refactored SharedPreferences access Access global SharedPreferences by calling PrefsManager.getInstance().getPrefs()

---
+++ app/src/main/java/ca/cmpt276/prj/model/PrefsManager.java
@@ -0,0 +1,33 @@
+package ca.cmpt276.prj.model;
+
+import android.content.SharedPreferences;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PrefsManager {
+	private SharedPreferences prefs;
+
+	// Singleton setup
+	private static PrefsManager instance;
+
+	private PrefsManager(SharedPreferences sharedPrefs) {
+		this.prefs = sharedPrefs;
+	}
+
+	public static PrefsManager getInstance() {
+		assertNotNull(instance);
+		return instance;
+	}
+
+	public static PrefsManager getInstance(SharedPreferences prefs) {
+		if (instance == null) {
+			instance = new PrefsManager(prefs);
+		}
+		return instance;
+	}
+	// End singleton setup
+
+	public SharedPreferences getPrefs() {
+		return prefs;
+	}
+}
