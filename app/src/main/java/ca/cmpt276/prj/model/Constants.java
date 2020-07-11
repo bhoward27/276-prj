e15108127a356dbf0c884516adb4fb02c149ecec
whorvath <whorvath@sfu.ca>
2020-07-11 02:14:46 -0700

    implemented image randomization, text for cards remaining general refactoring created new class to generate random positions

--- app/src/main/java/ca/cmpt276/prj/model/Constants.java
+++ app/src/main/java/ca/cmpt276/prj/model/Constants.java
@@ -7,12 +7,11 @@ public final class Constants {
 
 	public static final int LANDSCAPE_SET = 1;
 	public static final int PREDATOR_SET = 2;
-	public static final boolean DISCARD_PILE_TAPPED = true;
-	public static final boolean DRAW_PILE_TAPPED = false;
+	public static final boolean DISCARD_PILE = true;
+	public static final boolean DRAW_PILE = false;
 	public static final int NUM_HIGH_SCORES = 5;
 	public static final int MAX_NAME_LENGTH = 12;
 	public static final String PREFS = "ca.cmpt276.prj.Eleven";
 	// TODO: add functionality for more than 3 images (possible future iterations)
 	public static final int NUM_IMAGES = 3;
-
 }
--- app/src/main/java/ca/cmpt276/prj/ui/GameActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/GameActivity.java
@@ -10,34 +10,35 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.AbsoluteLayout;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import java.util.Stack;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.List;
 
 import ca.cmpt276.prj.R;
 import ca.cmpt276.prj.model.CardImage;
 import ca.cmpt276.prj.model.Game;
+import ca.cmpt276.prj.model.GenRand;
 import ca.cmpt276.prj.model.ScoreManager;
 
-import static ca.cmpt276.prj.model.Constants.DISCARD_PILE_TAPPED;
-import static ca.cmpt276.prj.model.Constants.DRAW_PILE_TAPPED;
+import static ca.cmpt276.prj.model.Constants.DISCARD_PILE;
+import static ca.cmpt276.prj.model.Constants.DRAW_PILE;
 import static ca.cmpt276.prj.model.Constants.LANDSCAPE_SET;
 import static ca.cmpt276.prj.model.Constants.NUM_IMAGES;
 
-
 public class GameActivity extends AppCompatActivity {
     public static final String TAG = "%%%GAMEACTIVITY";
-    ImageButton[] btnDisc = new ImageButton[3];
-    ImageButton[] btnDraw = new ImageButton[3];
-    Stack<Integer> rndXPos = new Stack<>();
-    Stack<Integer> rndYPos = new Stack<>();
+    List<ImageButton> discPileButtons = new ArrayList<>();
+    List<ImageButton> drawPileButtons = new ArrayList<>();
+    List<ImageButton> allButtons = new ArrayList<>();
+    List<Integer> rndXPos = new ArrayList<>();
+    List<Integer> rndYPos = new ArrayList<>();
+    int randCount = 0;
     ScoreManager scoreManager;
     Game gameInstance;
     String resourcePrefix;
@@ -50,20 +51,50 @@ public class GameActivity extends AppCompatActivity {
         initGame();
     }
 
-    private void setupDecorations() {
+    private void setupTimer() {
         Chronometer scoreTimer = findViewById(R.id.chrnTimerForScoring);
         scoreTimer.setBase(SystemClock.elapsedRealtime());
         scoreTimer.start();
     }
 
-    private void getButtons() {
-        btnDisc[0] = findViewById(R.id.btnDiscIm1);
-        btnDisc[1] = findViewById(R.id.btnDiscIm2);
-        btnDisc[2] = findViewById(R.id.btnDiscIm3);
+    private void initGame() {
+        scoreManager = ScoreManager.getInstance();
+        // TODO: we need options to be working to set the image set dynamically here
+        gameInstance = new Game(NUM_IMAGES, LANDSCAPE_SET);
 
-        btnDraw[0] = findViewById(R.id.btnDrawIm1);
-        btnDraw[1] = findViewById(R.id.btnDrawIm2);
-        btnDraw[2] = findViewById(R.id.btnDrawIm3);
+        // we have to wait until the cardview finishes loading before getting the positions for the
+        // images
+        CardView cardView = findViewById(R.id.crdDiscPile);
+        cardView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                updateRemainingCards();
+                cardView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                setupButtons();
+                setupTimer();
+            }
+        });
+    }
+
+    private void getButtons() {
+        discPileButtons.add(findViewById(R.id.btnDiscIm1));
+        discPileButtons.add(findViewById(R.id.btnDiscIm2));
+        discPileButtons.add(findViewById(R.id.btnDiscIm3));
+
+        drawPileButtons.add(findViewById(R.id.btnDrawIm1));
+        drawPileButtons.add(findViewById(R.id.btnDrawIm2));
+        drawPileButtons.add(findViewById(R.id.btnDrawIm3));
+
+        for (ImageButton button : discPileButtons) {
+            button.setTag(R.string.tag_btn_key, DISCARD_PILE);
+        }
+
+        for (ImageButton button : drawPileButtons) {
+            button.setTag(R.string.tag_btn_key, DRAW_PILE);
+        }
+
+        allButtons.addAll(discPileButtons);
+        allButtons.addAll(drawPileButtons);
     }
 
     private void refreshButtons() {
@@ -72,79 +103,60 @@ public class GameActivity extends AppCompatActivity {
 
         String resourceName;
         int resourceID;
-        int btnNumber = 0;
-        for (CardImage discImage : gameInstance.getDiscardPileImages()) {
-            resourceName = resourcePrefix + discImage.name().toLowerCase();
+        List<CardImage> discPileImages = gameInstance.getDiscardPileImages();
+        List<CardImage> drawPileImages = gameInstance.getDrawPileImages();
+
+        CardImage image;
+        for (ImageButton button : allButtons) {
+            int index = allButtons.indexOf(button);
+            if ((boolean) button.getTag(R.string.tag_btn_key) == DISCARD_PILE) {
+                image = discPileImages.get(index);
+            } else {
+                image = drawPileImages.get(index - gameInstance.getImagesPerCard());
+            }
+            resourceName = resourcePrefix + image.name().toLowerCase();
             resourceID = getResources().getIdentifier(resourceName, "drawable", getPackageName());
-            btnDisc[btnNumber].setImageResource(resourceID);
-            btnDisc[btnNumber].setTag(discImage);
-
-            AbsoluteLayout.LayoutParams pos = (AbsoluteLayout.LayoutParams) btnDisc[btnNumber].getLayoutParams();
-            pos.x = rndXPos.pop();
-            pos.y = rndYPos.pop();
-            btnDisc[btnNumber].setLayoutParams(pos);
-
-            // debug
-            Log.d("%%%GAMEACTIVITY", "resourceName: " + resourceName);
-            Log.d(TAG, "button tag: (refresh) " + (CardImage) btnDisc[btnNumber].getTag());
-            btnNumber++;
+            button.setImageResource(resourceID);
+            button.setTag(image);
+            // TODO: change to relative layout
+            AbsoluteLayout.LayoutParams pos = (AbsoluteLayout.LayoutParams) button.getLayoutParams();
+            pos.x = rndXPos.get(randCount + index);
+            pos.y = rndYPos.get(randCount + index);
+            button.setLayoutParams(pos);
         }
 
-        btnNumber = 0;
-        for (CardImage drawImage : gameInstance.getDrawPileImages()) {
-            resourceName = resourcePrefix + drawImage.name().toLowerCase();
-            resourceID = getResources().getIdentifier(resourceName, "drawable", getPackageName());
-            btnDraw[btnNumber].setImageResource(resourceID);
-            btnDraw[btnNumber].setTag(drawImage);
-
-            AbsoluteLayout.LayoutParams pos = (AbsoluteLayout.LayoutParams) btnDraw[btnNumber].getLayoutParams();
-            pos.x = rndXPos.pop();
-            pos.y = rndYPos.pop();
-            btnDraw[btnNumber].setLayoutParams(pos);
-
-            // debug
-            Log.d(TAG, "resourceName: " + resourceName);
-            Log.d(TAG, "button tag: (refresh) " + (CardImage) btnDraw[btnNumber].getTag());
-            btnNumber++;
-        }
-
+        randCount += gameInstance.getImagesPerCard();
     }
 
     @SuppressLint("ClickableViewAccessibility")
     public void setupButtons() {
+        // get global access to button ids
+        getButtons();
         // this function creates the array of button positions which will be used
         // for each image/card
         setupButtonPositions();
         // this function adds images and tags to the buttons
         refreshButtons();
 
-        for (ImageButton imageButton : btnDisc) {
-            imageButton.setOnTouchListener((ignored, motionEvent) -> {
+        for (ImageButton button : allButtons) {
+            button.setOnTouchListener((ignored, motionEvent) -> {
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    imageButton.setColorFilter(getColor(R.color.colorGreenFilter));
+                    button.setColorFilter(getColor(R.color.colorGreenFilter));
                 } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-
-                    resetOverlay(DISCARD_PILE_TAPPED, imageButton);
-                    tapUpdateGameState(DISCARD_PILE_TAPPED, imageButton);
+                    boolean pile = (boolean) button.getTag(R.string.tag_btn_key);
+                    resetOverlay(pile, button);
+                    tapUpdateGameState(pile, button);
                 }
 
                 return false;
             });
         }
 
-        for (ImageButton imageButton : btnDraw) {
-            imageButton.setOnTouchListener((ignored, motionEvent) -> {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    imageButton.setColorFilter(getColor(R.color.colorGreenFilter));
-                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    resetOverlay(DRAW_PILE_TAPPED, imageButton);
-                    tapUpdateGameState(DRAW_PILE_TAPPED, imageButton);
-                }
+    }
 
-                return false;
-            });
-        }
-
+    private void updateRemainingCards() {
+        TextView txtRemaining = findViewById(R.id.txtCardsRemaining);
+        txtRemaining.setText(getString(R.string.txt_cards_remaining) + " " + gameInstance.getRemainingCards());
     }
 
     private void setupButtonPositions() {
@@ -158,10 +170,13 @@ public class GameActivity extends AppCompatActivity {
         int widthMax = cardWidth - imageButtonWidth;
         int heightMax = cardHeight - imageButtonHeight;
 
-        int totalImages = gameInstance.getImagesPerCard()*gameInstance.getDeck().getTotalNumCards();
-        for (int i = 0; i < totalImages; i++) {
-            rndXPos.push(ThreadLocalRandom.current().nextInt(0, widthMax));
-            rndYPos.push(ThreadLocalRandom.current().nextInt(0, heightMax));
+        int totalCards = gameInstance.getDeck().getTotalNumCards();
+
+        for (int card = 0; card < totalCards; card++) {
+            GenRand gen = new GenRand(imageButtonWidth, imageButtonHeight, widthMax, heightMax, gameInstance.getImagesPerCard());
+            gen.generate();
+            rndXPos.addAll(gen.getxList());
+            rndYPos.addAll(gen.getyList());
         }
     }
 
@@ -175,9 +190,10 @@ public class GameActivity extends AppCompatActivity {
             Log.d(TAG, "tapUpdateGameState: match");
             if (!gameInstance.isGameOver()) {
                 // then change the images and remove the overlay to signify no card being selected
+                updateRemainingCards();
                 refreshButtons();
-                resetOverlay(DISCARD_PILE_TAPPED, null);
-                resetOverlay(DRAW_PILE_TAPPED, null);
+                resetOverlay(DISCARD_PILE, null);
+                resetOverlay(DRAW_PILE, null);
             } else {
                 finishGame();
             }
@@ -186,7 +202,7 @@ public class GameActivity extends AppCompatActivity {
 
     private void resetOverlay(boolean pile, ImageButton pressedButton) {
         // remove the overlays for all other buttons in the same card
-        for (ImageButton imageButton : (pile == DISCARD_PILE_TAPPED ? btnDisc : btnDraw)) {
+        for (ImageButton imageButton : (pile == DISCARD_PILE ? discPileButtons : drawPileButtons)) {
             if (imageButton != pressedButton) {
                 imageButton.setColorFilter(0);
             }
@@ -224,25 +240,6 @@ public class GameActivity extends AppCompatActivity {
         alert.show();
         // don't let user touch outside dialog box after game finished
         alert.setCanceledOnTouchOutside(false);
-    }
-
-    private void initGame() {
-        scoreManager = ScoreManager.getInstance();
-        // TODO: we need options to be working to set the image set dynamically here
-        gameInstance = new Game(NUM_IMAGES, LANDSCAPE_SET);
-
-        getButtons();
-        // we have to wait until the cardview finishes loading before getting the positions for the
-        // images
-        CardView cardView = findViewById(R.id.crdDiscPile);
-        cardView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                cardView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                setupButtons();
-                setupDecorations();
-            }
-        });
     }
 
 }
