a569902ccbf9754ae5e13961e83e1c34b2e87d0b
Benjamin Howard <bwhoward@sfu.ca>
2020-07-11 20:15:20 -0700

    Add relevant titles for all activities and add empty CreditsActivity.
    Change name for button that will launch CreditsActivity.

---
+++ app/src/main/java/ca/cmpt276/prj/ui/CreditsActivity.java
@@ -0,0 +1,16 @@
+package ca.cmpt276.prj.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.prj.R;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
