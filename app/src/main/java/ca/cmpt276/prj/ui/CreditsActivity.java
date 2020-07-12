5ae0930762a0116fa8a2ce09d2c5b82950867999
Benjamin Howard <bwhoward@sfu.ca>
2020-07-12 12:00:26 -0700

    Add source links to predator images in CreditsActivity.

--- app/src/main/java/ca/cmpt276/prj/ui/CreditsActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/CreditsActivity.java
@@ -30,10 +30,18 @@ public class CreditsActivity extends AppCompatActivity {
         TextView sunriseLink = (TextView) findViewById(R.id.textViewSunriseSource);
         TextView volcanoLink = (TextView) findViewById(R.id.textViewVolcanoSource);
 
+        TextView bearLink = (TextView) findViewById(R.id.textViewBearSource);
+        TextView eagleLink = (TextView) findViewById(R.id.textViewEagleSource);
+        TextView leopardLink = (TextView) findViewById(R.id.textViewLeopardSource);
+        TextView lionLink = (TextView) findViewById(R.id.textViewLionSource);
+        TextView sharkLink = (TextView) findViewById(R.id.textViewSharkSource);
+        TextView snakeLink = (TextView) findViewById(R.id.textViewSnakeSource);
+        TextView spiderLink = (TextView) findViewById(R.id.textViewSpiderSource);
 
-
-        TextView links[] = { caveLink, desertLink, forestLink, oceanLink, stormLink, sunriseLink,
-                                volcanoLink };
+        TextView links[] = { caveLink, desertLink, forestLink, oceanLink,
+                stormLink, sunriseLink, volcanoLink,
+                bearLink, eagleLink, leopardLink, lionLink,
+                sharkLink, snakeLink, spiderLink };
         //  CITATION: the below code in this method is based off of an answer on StackOverflow:
         //  https://stackoverflow.com/questions/2734270/how-to-make-links-in-a-textview-clickable
         for (TextView link : links) {
