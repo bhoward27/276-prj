9181c402f0fe7516b194e5f1713c0da9ae3c78c1
Benjamin Howard <bwhoward@sfu.ca>
2020-07-11 21:29:52 -0700

    Add hyperlinks for image sources to strings.xml.
    Add (incomplete) setupHyperlinks method to CreditsActivity. Rename the
    magnifying glass string resource.

--- app/src/main/java/ca/cmpt276/prj/ui/CreditsActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/CreditsActivity.java
@@ -3,6 +3,8 @@ package ca.cmpt276.prj.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import ca.cmpt276.prj.R;
 
@@ -12,5 +14,20 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        //setupHyperlinks();
+    }
+
+    private void setupHyperlinks() {
+        //  Create the textviews with findViewByID
+
+        TextView links[] = {/*the textViews with links*/};
+        //  CITATION: the below code in this method is based off of an answer on StackOverflow:
+        //  https://stackoverflow.com/questions/2734270/how-to-make-links-in-a-textview-clickable
+        for (TextView link : links) {
+            //  This makes it so that when a hyperlink is tapped, it presents an option
+            //  to the user to open the link in a web browser.
+            link.setMovementMethod(LinkMovementMethod.getInstance());
+        }
     }
 }
