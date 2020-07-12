dc786308d5fbc574fdfa97e26b68aea8143193d6
Benjamin Howard <bwhoward@sfu.ca>
2020-07-12 10:54:57 -0700

    Add landscape image links to CreditsActivity.
    Wire up credits button in HelpActivity, make image links launch web browser on
    tapping.

--- app/src/main/java/ca/cmpt276/prj/ui/CreditsActivity.java
+++ app/src/main/java/ca/cmpt276/prj/ui/CreditsActivity.java
@@ -2,6 +2,8 @@ package ca.cmpt276.prj.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
@@ -15,13 +17,23 @@ public class CreditsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
 
-        //setupHyperlinks();
+        setupHyperlinks();
     }
 
     private void setupHyperlinks() {
         //  Create the textviews with findViewByID
+        TextView caveLink = (TextView) findViewById(R.id.textViewCaveSource);
+        TextView desertLink = (TextView) findViewById(R.id.textViewDesertSource);
+        TextView forestLink = (TextView) findViewById(R.id.textViewForestSource);
+        TextView oceanLink = (TextView) findViewById(R.id.textViewOceanSource);
+        TextView stormLink = (TextView) findViewById(R.id.textViewStormSource);
+        TextView sunriseLink = (TextView) findViewById(R.id.textViewSunriseSource);
+        TextView volcanoLink = (TextView) findViewById(R.id.textViewVolcanoSource);
 
-        TextView links[] = {/*the textViews with links*/};
+
+
+        TextView links[] = { caveLink, desertLink, forestLink, oceanLink, stormLink, sunriseLink,
+                                volcanoLink };
         //  CITATION: the below code in this method is based off of an answer on StackOverflow:
         //  https://stackoverflow.com/questions/2734270/how-to-make-links-in-a-textview-clickable
         for (TextView link : links) {
@@ -29,5 +41,9 @@ public class CreditsActivity extends AppCompatActivity {
             //  to the user to open the link in a web browser.
             link.setMovementMethod(LinkMovementMethod.getInstance());
         }
+    }
+
+    public static Intent makeIntent(Context c) {
+         return new Intent(c, CreditsActivity.class);
     }
 }
