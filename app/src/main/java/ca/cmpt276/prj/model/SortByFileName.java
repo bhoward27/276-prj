5398cd7ec22ef69d8f14cc3835afdd4cf6354c38
whorvath <whorvath@sfu.ca>
2020-07-28 20:39:08 -0700

    Change comment

--- app/src/main/java/ca/cmpt276/prj/model/SortByFileName.java
+++ app/src/main/java/ca/cmpt276/prj/model/SortByFileName.java
@@ -5,15 +5,19 @@ import java.util.Comparator;
 
 import static ca.cmpt276.prj.model.Constants.JPG_EXTENSION;
 
+/**
+ * This is a helper class used for sorting in ascending order of downloaded Flickr file names
+ *
+ */
+
 public class SortByFileName implements Comparator<File> {
-	// Used for sorting in ascending order of
-	// roll number
 	public int compare(File a, File b)
 	{
 		String aName = a.getName();
 		String bName = b.getName();
 		int aInt = Integer.parseInt(aName.substring(2, aName.length()-JPG_EXTENSION.length()));
 		int bInt = Integer.parseInt(bName.substring(2, bName.length()-JPG_EXTENSION.length()));
+
 		return aInt - bInt;
 	}
 }
